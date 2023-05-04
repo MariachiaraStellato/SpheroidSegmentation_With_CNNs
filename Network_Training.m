2a238cfd915e0ef806847e97f1d0b03b90a0089c
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-05-04 11:04:58 +0200

    created code to prepare the datastore to be used for the training

--- Network_Training.m
+++ Network_Training.m
@@ -1,5 +1,25 @@
-function net = Network_Training()
+function net = Network_Training(ImagesDir, MasksDir)
 
+ 
+
+    a = 500; 
+    b = 500; 
+    TempImDirName = FUNC.process_images(ImagesDir,[a b], 1);
+    TempMaskDirName = FUNC.resize_images(MasksDir,[a,b]);
+    [dsTrain, dsVal, dsTest] = FUNC.Dataset_processing(TempImDirName,TempMaskDirName);
+    classes = [
+    "Sferoids"
+    "Background"
+    ]; %categories i wanto to segment the images into
+
+    I = readimage(dsTrain.UnderlyingDatastores{1,1},1);
+
+    [w, h, c] = size(I);
+    imageSize = [w, h, c]; %size of the images we are training 
+
+    numClasses = numel(classes); %number of classes I want to train for the segmentation 
+
+    numTrainingImages = numel(dsTrain.UnderlyingDatastores{1,1}.Files);
 
 
 end
