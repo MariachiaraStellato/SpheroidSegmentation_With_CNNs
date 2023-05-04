0828eb28a247de77454a933402585123cfd71e04
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-05-04 11:03:04 +0200

    created Dataset_processing function

--- FUNC.m
+++ FUNC.m
@@ -134,6 +134,119 @@ if exist('done', 'var')
     end
 end
 end
+%-----------------------------------------------------------------------------------------------------
+function [dsTrain, dsVal, dsTest] = Dataset_processing(ImagesDir,MasksDir)
+% AUTHOR: Mariachiara Stellato (E-mail: mariachiarastellato@gmail.com)
+% DATE: september 2022, 13
+%
+% This function divide the image datastore given as input and create the training dataset, validation dataset and testing
+% that will be used to train a CNN network by assigning 
+% to every image in the dataset the labels specified in "classes"
+% INPUT:
+% imagesDir:        is a string containing the directory of the images
+%                   folders 
+% MasksDir:         is a string containing the directory of the masks folders on
+%                   which the function will base the labels
+% OUTPUT:
+% dsTrain: pixel label datastore containing 80% of the input images
+% dsTest: pixel label datastore containing 10% of the input images
+% dsVal: pixel label datastore containing 10% of the input images
+%   
+% MiAi (Microscopy & Artificial Intelligence) Toolbox
+% Copyright © 2022 Mariachiara Stellato, Filippo Piccinini,   
+% University of Bologna, Italy. All rights reserved.
+%
+% This program is free software; you can redistribute it and/or modify it 
+% under the terms of the GNU General Public License version 3 (or higher) 
+% as published by the Free Software Foundation. This program is 
+% distributed WITHOUT ANY WARRANTY; without even the implied warranty of 
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+% General Public License for more details.
+
+% import images 
+imagesFolderName = ImagesDir;
+maskFolderName = MasksDir;
+masks = imageDatastore(maskFolderName, ...
+    'IncludeSubfolders',true, ...
+    'LabelSource','foldernames'); 
+image = imageDatastore(imagesFolderName, ...
+    'IncludeSubfolders',true, ...
+    'LabelSource','foldernames'); 
+
+datcre = msgbox('Datastore created');
+if exist('datcre', 'var')
+     if ishandle(datcre)
+           pause(2*eps);
+           delete(datcre)
+     end
+end
+% Labels
+
+I = readimage(image,1);
+originalImSize = size(I);
+%figure, imshow(I)
+
+classes = [
+    "Sferoids"
+    "Background"
+]; %categories i wanto to segment the images into 
+
+labelIDs = {255; 
+            0}; %graylevel to refer to the labels 
+
+pxds = pixelLabelDatastore(maskFolderName,classes,labelIDs,'IncludeSubfolders',true); 
+%create a datastore where every image pixel is associateed to the right
+%label 
+
+
+cmap = [
+    0 0 0  %background
+    1 1 1  %Sferoids
+];
+%clour map 
+
+
+tbl = countEachLabel(pxds);
+%count number of times the label appear in the dataset 
+
+
+labcre = msgbox('Label created');
+if exist('labcre', 'var')
+   if ishandle(labcre)
+       pause(2*eps);
+      delete(labcre)
+   end
+end
+% split the set in training,test and validation 
+
+% 80% for training, 10% for test, %10 for validation
+[imdsTrain,imdsValidation, imdsTest] = splitEachLabel(image,0.8,0.1); %immages 
+
+[maskTrain,maskValidation, maskTest] = splitEachLabel(masks,0.8,0.1); %masks
+
+pxdsTrain = pixelLabelDatastore(maskTrain.Files, classes, labelIDs);
+pxdsValidation = pixelLabelDatastore(maskValidation.Files, classes, labelIDs);
+pxdsTest = pixelLabelDatastore(maskTest.Files, classes, labelIDs);
+%create label dataset for training
+
+
+%combine the dataset of the images and the masks to associate the label to the images we need for the trining  
+dsVal = combine(imdsValidation,pxdsValidation);
+dsTrain = combine(imdsTrain, pxdsTrain);
+dsTest = combine(imdsTest,pxdsTest);
+
+numTrainingImages = numel(imdsTrain.Files);
+numTestingImages = numel(imdsTest.Files);
+
+datdiv = msgbox('Datastore divided');
+if exist('datdiv', 'var')
+    if ishandle(datdiv)
+        pause(2*eps);
+        delete(datdiv)
+    end
+end
+end
+%-------------------------------------------------------------------------------------------------
 
 
     end
