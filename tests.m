3a96d1c0bef2058583943eeb2c203e609a0dde75
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-05-10 11:01:28 +0200

    created function that tests the size of the image modified by the Process_images function

--- tests.m
+++ tests.m
@@ -8,6 +8,24 @@ classdef tests<matlab.unittest.TestCase
             c = 1;
             TempImDirName = process_images(ImagesFolderName, WantedSize, c);
             testCase.verifyEqual(exist(TempImDirName, 'dir'), 7);
+            rmdir(TempImDirName,'s');
+        end
+
+        function testImageSize(testCase)
+            ImagesFolderName = 'my_images_folder';
+            WantedSize = [256, 256];
+            c = 1;
+            TempImDirName = process_images(ImagesFolderName, WantedSize, c);
+            imageFiles = dir(fullfile(TempImDirName, '*.tiff'));
+            
+            % Verify image size for each resized image
+            for i = 1:numel(imageFiles)
+                I = imread(fullfile(TempImDirName, imageFiles(i).name));
+                imageSize = size(I);
+                expectedSize = [WantedSize, size(I, 3)]; % Add 3rd dimension for color images
+                testCase.verifyEqual(imageSize, expectedSize);
+            end
+            rmdir(TempImDirName,'s');
         end
     end
 end
