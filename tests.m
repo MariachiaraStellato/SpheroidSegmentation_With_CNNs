640c5fba630ad4752011a40cd6ef189c6e37063a
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-05-10 11:43:18 +0200

    Added resize_images testing

--- tests.m
+++ tests.m
@@ -6,7 +6,7 @@ classdef tests<matlab.unittest.TestCase
             ImagesFolderName = 'my_images_folder';
             WantedSize = [256, 256];
             c = 1;
-            TempImDirName = process_images(ImagesFolderName, WantedSize, c);
+            TempImDirName = FUNC.process_images(ImagesFolderName, WantedSize, c);
             testCase.verifyEqual(exist(TempImDirName, 'dir'), 7);
             rmdir(TempImDirName,'s');
         end
@@ -15,7 +15,7 @@ classdef tests<matlab.unittest.TestCase
             ImagesFolderName = 'my_images_folder';
             WantedSize = [256, 256];
             c = 1;
-            TempImDirName = process_images(ImagesFolderName, WantedSize, c);
+            TempImDirName = FUNC.process_images(ImagesFolderName, WantedSize, c);
             imageFiles = dir(fullfile(TempImDirName, '*.tiff'));
             
             % Verify image size for each resized image
@@ -26,6 +26,44 @@ classdef tests<matlab.unittest.TestCase
                 testCase.verifyEqual(imageSize, expectedSize);
             end
             rmdir(TempImDirName,'s');
+        end
+
+        %tests for the Resize_images function-------------------------------
+
+        function testResizeImages(testCase)
+        % Test the resize_images function with different input arguments
+
+        % Test case 1: Input folder contains no images
+        ImagesFolderName = 'non_existing_folder';
+        WantedSize = [256, 256];
+        assert(~exist(ImagesFolderName, 'dir'), 'Test folder already exists')
+        TempImDirName = resize_images(ImagesFolderName, WantedSize);
+        assert(~exist(TempImDirName, 'dir'), 'Temp folder created despite invalid input folder')
+        
+        % Test case 2: Input folder contains valid images
+        ImagesFolderName = 'sample_images';
+        WantedSize = [500, 500];
+        assert(exist(ImagesFolderName, 'dir'), 'Test folder does not exist')
+        TempImDirName = resize_images(ImagesFolderName, WantedSize);
+        assert(exist(TempImDirName, 'dir'), 'Temp folder not created')
+        assert(numel(dir(fullfile(TempImDirName, '*.tiff'))) == 2, 'Unexpected number of output images')
+
+        %test case 3: The images are correctly resized
+        
+        outputFiles = dir([TempImDirName '/*.tiff']);
+        numImages = length(outputFiles);
+        
+        % Loop over all images in the output directory and check their size
+        for i = 1:numImages
+            % Load the image and get its size
+            im = imread(fullfile(TempImDirName, outputFiles(i).name));
+            [h, w, ~] = size(im);
+            
+            % Check if the size matches the target size
+            verifyEqual(testCase, [h, w], WantedSize, 'AbsTol', 1, ...
+                ['Size of image ' outputFiles(i).name ' does not match the target size']);
+        end
+        rmdir(TempImDirName, 's');
         end
     end
 end
