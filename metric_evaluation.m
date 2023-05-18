0a8c4d813fe129df95aaeabc2c5741506f408dd4
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-05-18 15:05:31 +0200

    Update metric_evaluation.m

--- metric_evaluation.m
+++ metric_evaluation.m
@@ -1,3 +1,17 @@
-function metric_evaluation(imagesDir, TestMasksDir, SegmentedMaskDir, TrainedNet)
+function metrics = metric_evaluation(TestMasksDir, SegmentedMaskDir)
+
+classes = [
+    "Sferoids"
+    "Background"
+    ]; %categories i wanto to segment the images into
+
+labelIDs = {0; 
+            255}; %graylevel to refer to the labels 
+
+segmented = pixelLabelDatastore(SegmentedMaskDir,classes,labelIDs);
+
+expected = pixelLabelDatastore(TestMasksDir,classes,labelIDs);
+
+metrics = evaluateSemanticSegmentation(segmented,expected,'Verbose',false);
 
 end
