f897291173068f154104314fd4fbe899b1a4887b
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-05-18 15:07:00 +0200

    Update Main.m

--- Main.m
+++ Main.m
@@ -11,10 +11,16 @@ netname = [char(Choosen_Net),'_',T];
 netdir = fullfile(PathTrainedNet,netname);
 save(netdir,'net');
 msgbox('TRAINING: THE END.', 'Message');
+
 %% segmentation
 PathImageFolder = "\Your_images_Dir";
-PathImageFolderOut = "\Your_masks_Dir";
+PathImageFolderOut = "\Your_Segmented_Images_Dir";
 PathNetworkFolderInp = "\Your_network_Dir";
 SpecificImageName ="image_name";
 flag_ShowMask = 1;
 segmentation_multiple_images(PathImageFolderInp,PathImageFolderOut,PathNetworkFolderInp,SpecificImageName,flag_ShowMask);
+
+%% metric evaluation
+TestMaskDir = "\Your_masks_Dir";
+SegmentedMaskDir = "\Your_Segmented_Images_Dir";
+metrics = metric_evaluation(TestMasksDir, SegmentedMaskDir);
