92f3ae33191f2bbc072891f3263d2d5d6bd4d232
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-05-18 13:01:29 +0200

    Update Main.m

--- Main.m
+++ Main.m
@@ -1,4 +1,4 @@
-%%script to perform the training 
+%% script to perform the training 
 possible_net = ["VGG-16", "VGG-19", "ResNet-18","ResNet-50","ResNet101"];
 ImagesDir = "\Your_images_Dir";
 MasksDir = "\Your_masks_Dir";
@@ -11,3 +11,10 @@ netname = [char(Choosen_Net),'_',T];
 netdir = fullfile(PathTrainedNet,netname);
 save(netdir,'net');
 msgbox('TRAINING: THE END.', 'Message');
+%% segmentation
+PathImageFolder = "\Your_images_Dir";
+PathImageFolderOut = "\Your_masks_Dir";
+PathNetworkFolderInp = "\Your_network_Dir";
+SpecificImageName ="image_name";
+flag_ShowMask = 1;
+segmentation_multiple_images(PathImageFolderInp,PathImageFolderOut,PathNetworkFolderInp,SpecificImageName,flag_ShowMask);
