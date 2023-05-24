f952aeb1b7403bf9f5ef2ceca8ee8932b0667f80
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-05-24 14:21:55 +0200

    Update segmentation_multiple_images.m

--- segmentation_multiple_images.m
+++ segmentation_multiple_images.m
@@ -1,4 +1,40 @@
 function segmentation_multiple_images(ImFolder, SegFolder, network, specificImageName,flag_showmask)
+    % AUTHOR: Mariachiara Stellato (E-mail: mariachiarastellato@gmail.com)
+    % FUNCTION DESCRIPTION: 
+    %   this function segments a folder of images given as an input by using the
+    %   network given as an input. If specificImageName is empty the whole
+    %   folder is segmented, if a name is specified only the image with
+    %   that name will be segmented. 
+    %   if the flag_showmask == 1 the image and the relative mask will be
+    %   shown as a figure. 
+    %   WARNING: if in the SegFolder there is already a saved image with
+    %   the same name of a new segmentation, the new segmented image will
+    %   substitute the old one. 
+    % INPUTS:
+    %   ImFolder:               string containing the path of the folder containing
+    %                           the images we want to segment
+    %   SegFolder:              string containing the path of the folder in which 
+    %                           the segmented images will be saved
+    %   network:                directory of the .mat file containing the network to be
+    %                           used 
+    %   specificImageName:      for segmenting a single image, add the specific name of
+    %                           the image. If you want to segment all the images in the
+    %                           folder, add an empty space. 
+    %   flag_showmask:          if set to 1 the original image and the
+    %                           segmented one will be shown in the same maltab figure. 
+    
+    % MiAi (Microscopy & Artificial Intelligence) Toolbox
+    % Copyright © 2022 Mariachiara Stellato, Filippo Piccinini,   
+    % University of Bologna, Italy. All rights reserved.
+    %
+    % This program is free software; you can redistribute it and/or modify it 
+    % under the terms of the GNU General Public License version 3 (or higher) 
+    % as published by the Free Software Foundation. This program is 
+    % distributed WITHOUT ANY WARRANTY; without even the implied warranty of 
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+    % General Public License for more details.
+
+
     load(network, net);
 
     if isempty(specificImageName)
