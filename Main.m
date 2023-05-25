1d88fdbc6c824512ed76c7da5c04d425fbbc1d06
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-05-25 11:39:42 +0200

    Update Main.m

--- Main.m
+++ Main.m
@@ -1,11 +1,11 @@
 %% script to perform the training 
 possible_net = ["VGG-16", "VGG-19", "ResNet-18","ResNet-50","ResNet101"];
-ImagesDir = "\Your_images_Dir";
-MasksDir = "\Your_masks_Dir";
-PathTrainedNet = "\Your_network_Dir"; %The directory where the trained network will be saved 
+ImagesDir = "ExampleImages";
+MasksDir = "ExampleMasks";
+PathTrainedNet = "TrainedNetworks"; %The directory where the trained network will be saved 
 NetworkType = 5; %This correspond to the ResNet101 network
 Choosen_Net = possible_net(NetworkType);
-net = Network_training(ImagesDir, MasksDir, NetworkType);
+net = Network_Training(ImagesDir, MasksDir, NetworkType);
 T = datestr(now,'dd-mm-yy-HH-MM-SS');       
 netname = [char(Choosen_Net),'_',T]; 
 netdir = fullfile(PathTrainedNet,netname);
