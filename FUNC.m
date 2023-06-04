c840b3f7f4ecef33e2d92097be99f639392a176f
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-06-04 18:14:24 +0200

    Update FUNC.m

--- FUNC.m
+++ FUNC.m
@@ -377,68 +377,7 @@ classdef FUNC
         
         end
 %-------------------------------------------------------------------------------------------------------------------
-        function TempImDirName = resize_images(ImagesFolderName, WantedSize)
-            % AUTHOR: Mariachiara Stellato (E-mail: mariachiarastellato@gmail.com) 
-            % FUNCTION DESCRITPION: 
-            %   This function will get the images contained into the
-            %   ImagesFolderName folder, resize them to the wanted size and
-            %   save them in a newly created folder
-            % INPUT: 
-            %       imagesFolderName:  string containing the name of the 
-            %                          folder path of the images to process
-            %       WantedSize:        [a b] numerical array containing the
-            %                          size we want the new images to be resized to
-            %       
-            % OUTPUT:
-            %        TempImDirName: string containing the name of the new folder path where the resized images
-            %                       are stored 
-            
-            % MiAi (Microscopy & Artificial Intelligence) Toolbox
-            % Copyright © 2022 Mariachiara Stellato, Filippo Piccinini,   
-            % University of Bologna, Italy. All rights reserved.
-            %
-            % This program is free software; you can redistribute it and/or modify it 
-            % under the terms of the GNU General Public License version 3 (or higher) 
-            % as published by the Free Software Foundation. This program is 
-            % distributed WITHOUT ANY WARRANTY; without even the implied warranty of 
-            % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
-            % General Public License for more details.
         
-        %check to see if the folder was correctly defined
-        if not(isfolder(ImagesFolderName))
-            error('The folder name was not correctly defined')
-        end
-        
-        %load images
-        image = imageDatastore(ImagesFolderName, ...
-            'IncludeSubfolders',true, ...
-            'LabelSource','foldernames');
-        
-        numberOfImages = length(image.Files);
-        TempImDirName = append(ImagesFolderName, filesep, 'TempMask');
-        mkdir(TempImDirName);
-        for k = 1 : numberOfImages
-          % Get the file name of the images inside the folder.  
-          inputFileName = image.Files{k};
-          fprintf('%s\n', inputFileName);
-          I = imread(inputFileName);
-          if islogical(I)
-              I = uint8(I);
-          end
-          %resize the image 
-          I = imresize(I,WantedSize);
-          %save the images in the new folder
-          FUNC.ISaveImages(inputFileName,TempImDirName,I);
-        end
-        done = msgbox('Masks correctly processed!');
-        if exist('done', 'var')
-            if ishandle(done)
-               pause(2*eps);
-                delete(done)
-            end
-        end
-        end
-%-----------------------------------------------------------------------------------------------------
         function [dsTrain, dsVal, dsTest] = Dataset_processing(ImagesDir,MasksDir)
             % AUTHOR: Mariachiara Stellato (E-mail: mariachiarastellato@gmail.com)
             % DATE: september 2022, 13
