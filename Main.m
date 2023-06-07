4ef7c5d2b47346dba76102da91f06ce86fdba5af
MariachiaraStellato <mariachiarastellato@gmail.com>
2023-06-07 10:28:50 +0200

    Update Main.m
    Indentation of the code

--- Main.m
+++ Main.m
@@ -1,13 +1,13 @@
-%% script to perform the training 
+%% script to perform the training
 possible_net = ["VGG-16", "VGG-19", "ResNet-18","ResNet-50","ResNet101"];
 ImagesDir = "ExampleImages";
 MasksDir = "ExampleMasks";
-PathTrainedNet = "TrainedNetworks"; %The directory where the trained network will be saved 
+PathTrainedNet = "TrainedNetworks"; %The directory where the trained network will be saved
 NetworkType = 5; %This correspond to the ResNet101 network
 Choosen_Net = possible_net(NetworkType);
 net = Network_Training(ImagesDir, MasksDir, NetworkType);
-T = datestr(now,'dd-mm-yy-HH-MM-SS');       
-netname = [char(Choosen_Net),'_',T]; 
+T = datestr(now,'dd-mm-yy-HH-MM-SS');
+netname = [char(Choosen_Net),'_',T];
 netdir = fullfile(PathTrainedNet,netname);
 save(netdir,'net');
 msgbox('TRAINING: THE END.', 'Message');
